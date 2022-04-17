97d4c8a3feee9cca924e690aedafa5c28f8b806c
Tanat <ttangun1@gmail.com>
2022-04-17 22:55:23 +0700

    feat: implement simple plan

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -1,5 +1,17 @@
 package jp.jaxa.iss.kibo.rpc.defaultapk;
 
+import android.util.Log;
+
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+import gov.nasa.arc.astrobee.Kinematics;
+import gov.nasa.arc.astrobee.Result;
+import gov.nasa.arc.astrobee.types.Point;
+import gov.nasa.arc.astrobee.types.Quaternion;
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
 
 /**
@@ -7,9 +19,159 @@ import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
  */
 
 public class YourService extends KiboRpcService {
+
+    final int LOOP_MAX = 3;
+
+    final double[] CAM_MATSIM = {
+            661.783002, 0.000000, 595.212041,
+            0.000000, 671.508662, 489.094196,
+            0.000000, 0.000000, 1.000000
+    };
+
+    final double[] DIST_COEFFSIM = {
+            -0.215168, 0.044354, 0.003615, 0.005093, 0.000000
+    };
+
+    private void move_to(double x, double y, double z, Quaternion q) {
+        final Point p = new Point(x, y, z);
+
+        int counter = 0;
+        Result result;
+
+        do {
+            result = api.moveTo(p, q, true);
+            counter++;
+        } while (!result.hasSucceeded() && (counter < LOOP_MAX));
+
+    }
+
+    private Mat find_circle() {
+        Mat circle = new Mat();
+        Mat img = new Mat(api.getMatNavCam(), new Rect(550, 500, 300, 300));
+
+        Imgproc.HoughCircles(img, circle, Imgproc.HOUGH_GRADIENT, 1, 300, 50, 30, 0, 0);
+
+        org.opencv.core.Point center = new org.opencv.core.Point(circle.get(0, 0)[0], circle.get(0, 0)[1]);
+
+        if (circle.empty())
+            return null;
+
+        Mat res = new Mat(1, 2, CvType.CV_32FC2);
+        float[] point = {(float) center.x + 550, (float) center.y + 500};
+
+        res.put(0, 0, point);
+
+        return res;
+    }
+
+    private Mat undistortPoints(Mat points) {
+        // in -> rows:1, cols:4
+        // in -> 1xN 2 Channel
+
+        Mat cameraMat = new Mat(3, 3, CvType.CV_32FC1);
+        Mat distCoeffs = new Mat(1, 5, CvType.CV_32FC1);
+
+        cameraMat.put(0, 0, CAM_MATSIM);
+        distCoeffs.put(0, 0, DIST_COEFFSIM);
+
+        Mat out = new Mat(points.rows(), points.cols(), points.type());
+
+        Imgproc.undistortPoints(points, out, cameraMat, distCoeffs, new Mat(), cameraMat);
+
+        // out -> 1xN 2 Channel
+        return out;
+    }
+
+    private double[] pixelDistanceToAngle(double[] target, double[] ref) {
+
+        final double xDistance = target[0] - ref[0];
+        final double yDistance = ref[1] - target[1];
+        //final double anglePerPixel = 130 / Math.sqrt(Math.pow(NAV_MAX_WIDTH, 2) + Math.pow(NAV_MAX_HEIGHT, 2));
+        final double anglePerPixel = 0.08125;
+
+        double xAngle = xDistance * anglePerPixel;
+        double yAngle = yDistance * anglePerPixel;
+
+        double[] out = {xAngle, yAngle};
+        return out;
+    }
+
+    private Quaternion eulerAngleToQuaternion(double xAngle, double yAngle, double zAngle) {
+        xAngle = Math.toRadians(xAngle);
+        yAngle = Math.toRadians(yAngle);
+        zAngle = Math.toRadians(zAngle);
+        double c1 = Math.cos(yAngle/2);
+        double c2 = Math.cos(zAngle/2);
+        double c3 = Math.cos(xAngle/2);
+        double s1 = Math.sin(yAngle/2);
+        double s2 = Math.sin(zAngle/2);
+        double s3 = Math.sin(xAngle/2);
+
+        double w = c1*c2*c3 - s1*s2*s3;
+        double x = s1*s2*c3 + c1*c2*s3;
+        double y = s1*c2*c3 + c1*s2*s3;
+        double z = c1*s2*c3 - s1*c2*s3;
+
+        return new Quaternion((float)x, (float) y, (float)z, (float)w);
+    }
+
+
+    private Quaternion combineQuaternion(Quaternion newOrientation, Quaternion oldOrientation) {
+        // For multiply quaternion, Apply q2(new) to q1(old)
+        // q1 * q2= a*e - b*f - c*g- d*h + i (b*e + a*f + c*h - d*g) + j (a*g - b*h + c*e + d*f) + k (a*h + b*g - c*f + d*e)
+
+        double x =  newOrientation.getX() * oldOrientation.getW() + newOrientation.getY() * oldOrientation.getZ()
+                - newOrientation.getZ() * oldOrientation.getY() + newOrientation.getW() * oldOrientation.getX();
+        double y = -newOrientation.getX() * oldOrientation.getZ() + newOrientation.getY() * oldOrientation.getW()
+                + newOrientation.getZ() * oldOrientation.getX() + newOrientation.getW() * oldOrientation.getY();
+        double z =  newOrientation.getX() * oldOrientation.getY() - newOrientation.getY() * oldOrientation.getX()
+                + newOrientation.getZ() * oldOrientation.getW() + newOrientation.getW() * oldOrientation.getZ();
+        double w = -newOrientation.getX() * oldOrientation.getX() - newOrientation.getY() * oldOrientation.getY()
+                - newOrientation.getZ() * oldOrientation.getZ() + newOrientation.getW() * oldOrientation.getW();
+
+        return new Quaternion((float)x, (float)y, (float)z, (float)w);
+    }
+
+
     @Override
     protected void runPlan1(){
-        // write here your plan 1
+        api.startMission();
+
+        // move to point A
+        Quaternion quaternion = new Quaternion(0f, 0.707f, 0f, 0.707f);
+        move_to(10.71, -7.7, 4.48, quaternion);
+        api.reportPoint1Arrival();
+
+        // shoot laser
+        move_to(10.71, -7.811971 , 4.48, quaternion);
+        api.laserControl(true);
+        api.takeTarget1Snapshot();
+
+
+        // move to point 2
+        quaternion = new Quaternion(0f, 0f, -0.707f, 0.707f);
+        move_to(10.876214285714285, -8.5, 4.97063, quaternion);
+        move_to(11.0067, -9.44819, 5.186407142857143, quaternion);
+        move_to(11.2746, -9.92284, 5.29881, quaternion);
+
+        // shoot laser
+        final Mat target = find_circle();
+        final Mat undistort_target = undistortPoints(target);
+        final double[] laser = {711, 455};
+        final double[] angleToTurn = pixelDistanceToAngle(undistort_target.get(0, 0), laser);
+        final Quaternion imageQ = eulerAngleToQuaternion(angleToTurn[1], 0, angleToTurn[0]);
+        final Quaternion qToTurn  = combineQuaternion(imageQ, new Quaternion(0, 0, -0.707f, 0.707f));
+
+        move_to(11.2746, -9.92284, 5.29881, qToTurn);
+        api.laserControl(true);
+        api.takeTarget2Snapshot();
+
+        // move to goal
+        move_to(11.0067, -9.44819, 5.1722, quaternion);
+        move_to(11.073773469387755, -8.5, 4.97063, quaternion);
+        move_to(11.2746, -7.89178, 4.96538, quaternion);
+
+        api.reportMissionCompletion();
     }
 
     @Override
